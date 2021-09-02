185c4bf9436be7f9e8bbd743e2ab747b5cb316d8
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-02 16:41:45 +0100

    #11 Saving the entry station

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,9 +9,11 @@ describe Oystercard do
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
+
     it 'can top up balance' do
       expect { subject.top_up(10) }.to change { subject.balance }.by (10)   
     end
+
     it 'raises an error if card beyond limit' do
       max_balance = Oystercard::MAX_BALANCE
       subject.top_up(max_balance)
@@ -25,27 +27,36 @@ describe Oystercard do
     end
   end
 
+  let(:station){ double :station }
   describe '#touch_in' do
     it 'can touch in' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject).to be_in_journey # true, alternative: expect(subject.in_journey?).to be true 
     end
+    
     it 'raises error if below minimum balance' do
-      expect{ subject.touch_in }.to raise_error "Insufficient balance"
+      expect{ subject.touch_in(station) }.to raise_error "Insufficient balance"
+    end
+
+    it 'records journey' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq (station) 
     end
   end
 
   describe '#touch_out' do
     it 'can touch out' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
       expect(subject).not_to be_in_journey # false alternative: expect(subject.in_journey?).to be false
     end
+    
     it 'reduce the balance by minimum fare' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station)
       expect { subject.touch_out }.to change {subject.balance}.by (-Oystercard::MIN_BALANCE) 
     end
   end
